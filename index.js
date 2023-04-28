4347a7063900304ed74671211c2b45e3f4afbe89
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-04-27 21:10:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(response => response.json())
+    .then(function(data){
+        const domElement = document.createElement('div')
+        document.querySelector('body').appendChild(domElement)
+        domElement.innerHTML = data.id
+    })
+    .catch(error => {
+        const errorMessage = error.message;
+        const errorElement = document.createElement('p');
+        errorElement.textContent = `Error: ${errorMessage}`;
+        document.body.appendChild(errorElement);
+      });
+}
